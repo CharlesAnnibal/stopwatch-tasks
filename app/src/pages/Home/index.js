09aa30729987188cc4b543a80f02757f90c1797f
unknown <charlesannibal@gmail.com>
2020-09-22 09:59:02 +0100

    implements cookie consent from lib react-cookie-consent

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -13,7 +13,8 @@ import StopWatchApp from "../../components/StopWatchApp/index.js";
 import TaskList from "../../components/TaskList/index.js";
 import InfoSection from "../../components/sections/content/InfoSection/index.js";
 import Footer from "../../components/sections/footer/index.js";
-import CookieConsent from "../../components/CookieConsent/index.js";
+import CookieConsent, { Cookies } from "react-cookie-consent";
+import { BLACK, WHITE, MAIN_COLOR_MEDIUM } from "../../constants/defaultStyleProps.js";
 export default class Home extends Component {
     constructor(props) {
         super(props);
@@ -40,8 +41,22 @@ export default class Home extends Component {
                     </Sidebar>
                 </FirstArticle>
                 <InfoSection />
-                <Footer/>
-                <CookieConsent/>
+                <Footer />
+                <CookieConsent
+                    location="bottom"
+                    buttonText="Accept"
+                    cookieName="myAwesomeCookieName2"
+                    style={{ background: BLACK }}
+                    buttonStyle={{ color: WHITE, fontSize: "13px", background: MAIN_COLOR_MEDIUM }}
+                    expires={150}
+                >
+                    This website uses cookies, which are necessary for its
+                    functioning and required to achieve the purposes of it. You
+                    accept the use of cookies or other identifiers by closing or
+                    dismissing this notice , by clicking on "Accept" or by
+                    continuing to browse otherwise.{" "}
+
+                </CookieConsent>
             </Wrapper>
         );
     }
